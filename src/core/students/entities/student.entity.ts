0c5191ba95d70e856b6dc1d0deef75c54bddbaf2
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2026-05-20 16:12:52 +0200

    Fix student entity primary key and relation to user entity

--- src/core/students/entities/student.entity.ts
+++ src/core/students/entities/student.entity.ts
@@ -1,20 +1,16 @@
-import {
-  Column,
-  Entity,
-  JoinColumn,
-  OneToOne,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
+import { Column, Entity, JoinColumn, OneToOne, PrimaryColumn } from 'typeorm';
 import { StudentStatus } from '../interfaces/student.interface';
 import { User } from '../../users/entities/user.entity';
 
 @Entity()
-export class Student extends User {
-  @PrimaryGeneratedColumn()
-  declare id: number;
-  @OneToOne(() => User, { cascade: true })
+export class Student {
+  @PrimaryColumn()
+  user_id: number;
+
+  @OneToOne(() => User, (user) => user.id, { cascade: true })
   @JoinColumn({ name: 'user_id' })
   user: User;
+
   @Column()
   program_id: number;
   @Column()
