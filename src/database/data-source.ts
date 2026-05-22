e94ae4e570ceedf09c2920cd9a6430908c7f4a85
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2026-05-22 09:11:52 +0200

    Update database type from MySQL to PostgreSQL
    Update docker-compose, ports, username & password, and remove all old
    migrations files

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -3,11 +3,11 @@ import { config } from 'dotenv';
 config(); // Load variables from your .env file
 
 export default new DataSource({
-  type: 'mysql',
+  type: 'postgres',
   host: process.env.DB_HOST_CLI || 'localhost',
-  port: parseInt(process.env.DB_PORT_CLI || '3306', 10),
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'secretpassword',
+  port: parseInt(process.env.DB_PORT_CLI || '5432', 10),
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
   database: process.env.DB_NAME || 'entropy_users_db',
   entities: ['src/**/*.entity{.ts,.js}'],
   migrations: ['src/database/migrations/*{.ts,.js}'],
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -8,11 +8,11 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
+        type: 'postgres',
         host: configService.get<string>('DB_HOST', 'localhost'),
-        port: configService.get<number>('DB_PORT', 3306),
-        username: configService.get<string>('DB_USER', 'root'),
-        password: configService.get<string>('DB_PASSWORD', 'secretpassword'),
+        port: configService.get<number>('DB_PORT', 5432),
+        username: configService.get<string>('DB_USER', 'postgres'),
+        password: configService.get<string>('DB_PASSWORD', 'postgres'),
         database: configService.get<string>('DB_NAME', 'entropy_users_db'),
         autoLoadEntities: true,
         synchronize: false, // VERY IMPORTANT when using migrations
