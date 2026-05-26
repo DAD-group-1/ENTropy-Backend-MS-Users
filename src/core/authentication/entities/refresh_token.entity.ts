5a25fd5b41bbb9b067c4657f67a76ed6276c9f55
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2026-05-26 14:41:46 +0200

    Fix RefreshToken property not being serializable by TypeORM

--- src/core/authentication/entities/refresh_token.entity.ts
+++ src/core/authentication/entities/refresh_token.entity.ts
@@ -21,7 +21,7 @@ export class RefreshToken {
   @Column()
   expires_at: Date;
   @Column({ nullable: true })
-  revoked_at: Date | null;
+  revoked_at?: Date;
 
   @CreateDateColumn()
   created_at: Date;
