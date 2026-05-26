ffbb1e24e9d7226e2d5b5170c8879711657289cc
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2026-05-26 11:54:53 +0200

    Use logger creation from common package

--- src/main.ts
+++ src/main.ts
@@ -2,36 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { ConfigService } from '@nestjs/config';
-import winston from 'winston';
-import { WinstonModule } from 'nest-winston';
+import { createWinstonLogger } from '@dad-group-1/backend-common';
 import * as dotenv from 'dotenv';
 
 dotenv.config(); // Load environment variables from .env
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: WinstonModule.createLogger({
-      level: 'info',
-      format: winston.format.json(),
-      defaultMeta: { service: 'user-service' },
-      transports: [
-        //
-        // - Write all logs with importance level of `error` or higher to `error.log`
-        //   (i.e., error, fatal, but not other levels)
-        //
-        new winston.transports.File({ filename: 'error.log', level: 'error' }),
-        //
-        // - Write all logs with importance level of `info` or higher to `combined.log`
-        //   (i.e., fatal, error, warn, and info, but not trace)
-        //
-        new winston.transports.File({ filename: 'combined.log' }),
-
-        //
-        // - Write all logs to the console as well
-        //
-        ...(process.env.ENVIRONMENT == 'DEV' ? [new winston.transports.Console({ format: winston.format.cli() })] : []),
-      ],
-    }),
+    logger: createWinstonLogger('user-service', 'info'),
   });
   const configService = app.get(ConfigService);
 
