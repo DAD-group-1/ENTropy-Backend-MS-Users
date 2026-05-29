7e8f0bb28b63182c1e0a86487bc3b6dec1b16682
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2026-05-29 14:56:48 +0200

    Fix console log in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { ConfigService } from '@nestjs/config';
 import { createWinstonLogger } from '@dad-group-1/backend-common';
+import * as dotenv from 'dotenv';
+import { Logger } from '@nestjs/common';
+
+dotenv.config(); // Load environment variables from .env
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -24,8 +28,8 @@ async function bootstrap() {
   await app.startAllMicroservices();
   await app.init(); // Initialize the app without starting the HTTP server
 
-  console.info(
-    `User microservice is listening on ${microserviceHost}:${microservicePort}`,
+  new Logger('Bootstrap').log(
+    `User microservice started on ${microserviceHost}:${microservicePort}`,
   );
 }
 bootstrap();
