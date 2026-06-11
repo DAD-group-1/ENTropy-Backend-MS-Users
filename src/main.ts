f892c3aa7806d6257a9615af092e5adcc1d54477
Red-Hide <guillaume.ferrand67@gmail.com>
2026-06-11 23:21:00 +0200

    Rabbitmq configuration

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ConfigService } from '@nestjs/config';
 import { createWinstonLogger } from '@dad-group-1/backend-common';
 import * as dotenv from 'dotenv';
 import { Logger } from '@nestjs/common';
+import { RmqUrl } from '@nestjs/microservices/external/rmq-url.interface';
 
 dotenv.config(); // Load environment variables from .env
 
@@ -14,22 +15,27 @@ async function bootstrap() {
   });
   const configService = app.get(ConfigService);
 
-  const microserviceHost = configService.get<string>('HOST', '0.0.0.0');
-  const microservicePort = configService.get<number>('PORT', 3001);
-
   app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.TCP,
+    transport: Transport.RMQ,
     options: {
-      host: microserviceHost,
-      port: microservicePort,
+      urls: [
+        {
+          username: configService.getOrThrow<string>('RABBITMQ_USERNAME'),
+          password: configService.getOrThrow<string>('RABBITMQ_PASSWORD'),
+          hostname: configService.getOrThrow<string>('RABBITMQ_HOST'),
+          port: configService.getOrThrow<number>('RABBITMQ_PORT'),
+        } as RmqUrl,
+      ],
+      queue: `USERS_SERVICE_QUEUE`,
+      queueOptions: {
+        durable: true,
+      },
     },
   });
 
   await app.startAllMicroservices();
   await app.init(); // Initialize the app without starting the HTTP server
 
-  new Logger('Bootstrap').log(
-    `User microservice started on ${microserviceHost}:${microservicePort}`,
-  );
+  new Logger('Bootstrap').log(`User microservice started`);
 }
 bootstrap();
